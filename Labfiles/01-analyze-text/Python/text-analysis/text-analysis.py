5ada4db9a635b28f428d0581407358f28da62fab
SwapnilBhusari <swapnil.r.bhusari@gmail.com>
2024-06-10 22:23:30 -0700

    01-working

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -11,8 +12,9 @@ def main():
         ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
         ai_key = os.getenv('AI_SERVICE_KEY')
 
-        # Create client using endpoint and key
-
+        # Create client using endpoint and keypython text-analysis.pypython text-analysis.py
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -23,20 +25,45 @@ def main():
             print('\n' + text)
 
             # Get language
-
+            detectedLanguages = ai_client.detect_language(documents=[text])
+            #print(type(detectedLanguages))
+            #print(detectedLanguages)
+            detectedLanguage = detectedLanguages[0]
+            print('\nLanguage:{}'.format(detectedLanguage.primary_language.name))
 
             # Get sentiment
+            sentimentAnalysisList = ai_client.analyze_sentiment(documents=[text])
+            #print(sentimentAnalysisList)
+            sentimentAnalysis = sentimentAnalysisList[0]
+            print("\nSentiment:{}".format(sentimentAnalysis.sentiment))
 
 
             # Get key phrases
-
+            phrasesList = ai_client.extract_key_phrases(documents=[text])
+            #print(phrasesList)
+            phrases = phrasesList[0].key_phrases 
+            if len(phrases) > 0:
+                print("\n Key Phrases:")
+                for phrase in phrases:
+                    print("\t{}".format(phrase))
 
             # Get entities
-
+            entitiesList = ai_client.recognize_entities(documents=[text])
+            #print(entitiesList)
+            entities = entitiesList[0].entities
+            if len(entities) > 0:
+                print("\nEntities:")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
             # Get linked entities
-
-
+            entitiesList = ai_client.recognize_linked_entities(documents=[text])
+            #print(entitiesList)
+            entities = entitiesList[0].entities
+            if len(entities) > 0:
+                print("\n Links:")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
     except Exception as ex:
         print(ex)
